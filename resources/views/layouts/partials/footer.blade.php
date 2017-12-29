8f175dd7f515b5488694876b2f1e2cb4964d4a1d
Gabriel Sturtevant <gsturtevant87@gmail.com>
2017-12-29 13:57:31 -0800

    Modified footer alignment and text

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,5 +1,10 @@
 <footer class="footer">
     <div class="container">
-        <span class="text-muted">Place sticky footer content here.</span>
+        <span class="text-muted">
+            See the code used for this website
+            <a href="https://github.com/GabrielSturtevant/laravel_personal_website" target="_blank">
+                here
+            </a>
+        </span>
     </div>
 </footer>
